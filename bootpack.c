2b103e3f717290c2bf251fc35666293942d3d9a9
cashev <42924021+cashev@users.noreply.github.com>
2020-05-03 17:09:28 +0900

    p76 ポインタを導入

--- bootpack.c
+++ bootpack.c
@@ -1,7 +1,6 @@
 /* 他のファイルで作った関数がありますとCコンパイラに教える */
 
 void io_hlt(void);
-void write_mem8(int addr, int data);
 
 /* 関数宣言なのに、{}がなくていきなり;を書くと、
     他のファイルにあるからよろしくね、という意味になるのです。 */
@@ -9,9 +8,14 @@ void write_mem8(int addr, int data);
 void HariMain(void)
 {
     int i; /* 変数宣言。iという変数は、32ビットの整数型 */
+    char *p; /* pという変数は、BYTE [...]用の番地 */
 
     for (i = 0xa0000; i <= 0xaffff; i++) {
-        write_mem8(i, i & 0x0f);
+
+	    p = (char *) i; /* 番地を代入 */
+	    *p = i & 0x0f;
+
+	    /* これで write_mem8(i, i & 0x0f); の代わりになる */
     }
 
     for (;;) {
