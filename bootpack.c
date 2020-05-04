63898bacd711ab751db47e6384058b790c6c5331
cashev <42924021+cashev@users.noreply.github.com>
2020-05-05 01:46:51 +0900

    fix typo

--- bootpack.c
+++ bootpack.c
@@ -19,7 +19,7 @@ void io_store_eflags(int eflags);
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
-void init_screen(char *vram, int x, int y);
+void init_screen8(char *vram, int x, int y);
 void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
 void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
 void init_mouse_cursor8(char *mouse, char bc);
@@ -75,7 +75,7 @@ void HariMain(void)
 
     init_gdtidt();
     init_palette(); /* パレットを設定 */
-    init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
+    init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
     init_mouse_cursor8(mcursor, COL8_008484);
 
     mx = (binfo->scrnx - 16) / 2; /* 画面中央になるように座標計算 */
