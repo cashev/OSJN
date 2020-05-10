9c4d31ae75c70ea2355cd489c482d1a765511b95
cashev <42924021+cashev@users.noreply.github.com>
2020-05-10 17:58:54 +0900

    p160 harib05b 整理

--- bootpack.c
+++ bootpack.c
@@ -3,9 +3,14 @@
 #include "bootpack.h"
 #include <stdio.h>
 
+struct MOUSE_DEC {
+  unsigned char buf[3], phase;
+};
+
 extern struct FIFO8 keyfifo, mousefifo;
 void enable_mouse(void);
 void init_keyboard(void);
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
 
 void HariMain(void)
 {
@@ -13,6 +18,7 @@ void HariMain(void)
   char s[40], mcursor[256], keybuf[32], mousebuf[128];
   int mx, my, i;
   unsigned char mouse_dbuf[3], mouse_phase;
+  struct MOUSE_DEC mdec;
 
   init_gdtidt();
   init_pic();
@@ -36,7 +42,6 @@ void HariMain(void)
   putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
   enable_mouse();
-  mouse_phase = 0; // マウスの0xfaを待っている段階へ
 
   for (;;) {
     io_cli();
@@ -52,25 +57,9 @@ void HariMain(void)
       } else if (fifo8_status(&mousefifo) != 0) {
         i = fifo8_get(&mousefifo);
         io_sti();
-        if (mouse_phase == 0) {
-          // マウスの0xfaを待っている段階
-          if (i == 0xfa) {
-            mouse_phase = 1;
-          }
-        } else if (mouse_phase == 1) {
-          // マウスの1バイト目を待っている段階
-          mouse_dbuf[0] = i;
-          mouse_phase = 2;
-        } else if (mouse_phase == 2) {
-          // マウスの2バイト目を待っている段階
-          mouse_dbuf[1] = i;
-          mouse_phase = 3;
-        } else if (mouse_phase == 3) {
-          // マウスの3バイト目を待っている段階
-          mouse_dbuf[2] = i;
-          mouse_phase = 1;
+        if (mouse_decode(&mdec, i) != 0) {
           // データが3バイト揃ったので表示
-          sprintf(s, "%02X %02X %02X", mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2]);
+          sprintf(s, "%02X %02X %02X", mdec.buf[0], mdec.buf[1], mdec.buf[2]);
           boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 8 * 8 - 1, 31);
           putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
         }
@@ -119,3 +108,34 @@ void enable_mouse(void)
   io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
   return; // うまくいくとACK(0xfa)が送信されてくる
 }
+
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
+{
+  if (mdec->phase == 0) {
+    // マウスの0xfaを待っている段階
+    if (dat == 0xfa) {
+      mdec->phase = 1;
+    }
+    return 0;
+  }
+  if (mdec->phase == 1) {
+    // マウスの1バイト目を待っている段階
+    mdec->buf[0] = dat;
+    mdec->phase = 2;
+    return 0;
+  }
+  if (mdec->phase == 2) {
+    // マウスの2バイト目を待っている段階
+    mdec->buf[1] = dat;
+    mdec->phase = 3;
+    return 0;
+  }
+  if (mdec->phase == 3) {
+    // マウスの3バイト目を待っている段階
+    mdec->buf[2] = dat;
+    mdec->phase = 1;
+    return 1;
+  }
+  return -1;
+}
+
