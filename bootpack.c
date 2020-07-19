ba281ee70d266402a91dc90b70bb7d955ca7bfd7
Cashew <42924021+cashev@users.noreply.github.com>
2020-07-19 07:15:14 -0700

    p209 harib07c 重ね合わせ処理の高速化

--- bootpack.c
+++ bootpack.c
@@ -58,7 +58,7 @@ void HariMain(void)
   sprintf(s, "memory %dMB   free : %dKB",
       memtotal / (1024 * 1024), memman_total(memman) / 1024);
   putfonts8_asc(buf_back, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
-  sheet_refresh(shtctl);
+  sheet_refresh(shtctl, sht_back, 0, 0, binfo->scrnx, 48);
 
   for (;;) {
     io_cli();
@@ -71,7 +71,7 @@ void HariMain(void)
         sprintf(s, "%02X", i);
         boxfill8(buf_back, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
         putfonts8_asc(buf_back, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
-        sheet_refresh(shtctl);
+        sheet_refresh(shtctl, sht_back, 0, 16, 16, 32);
       } else if (fifo8_status(&mousefifo) != 0) {
         i = fifo8_get(&mousefifo);
         io_sti();
@@ -89,6 +89,7 @@ void HariMain(void)
           }
           boxfill8(buf_back, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);
           putfonts8_asc(buf_back, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+          sheet_refresh(shtctl, sht_back, 32, 16, 32 + 15 * 8, 32);
 
           // マウスカーソルの移動
           mx += mdec.x;
@@ -108,7 +109,8 @@ void HariMain(void)
           sprintf(s, "(%3d, %3d)", mx, my);
           boxfill8(buf_back, binfo->scrnx, COL8_008484, 0, 0, 79, 15); // 座標を消す
           putfonts8_asc(buf_back, binfo->scrnx, 0, 0, COL8_FFFFFF, s); // 座標を書く
-          sheet_slide(shtctl, sht_mouse, mx, my); // sheet_refreshを含む
+          sheet_refresh(shtctl, sht_back, 0, 0, 80, 16);
+          sheet_slide(shtctl, sht_mouse, mx, my);
         }
       }
     }
