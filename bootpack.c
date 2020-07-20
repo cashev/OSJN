7f1114e7e00d1d246d034afec0c5aeebd2d9094e
Cashew <42924021+cashev@users.noreply.github.com>
2020-07-20 08:01:09 -0700

    p220 harib08d ウィンドウを出力

--- bootpack.c
+++ bootpack.c
@@ -3,6 +3,8 @@
 #include "bootpack.h"
 #include <stdio.h>
 
+void make_window8(unsigned char *buf, int xsize, int ysize, char *title);
+
 void HariMain(void)
 {
   struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO; // asmhead.nas の番地に合わせる
@@ -12,18 +14,16 @@ void HariMain(void)
   struct MOUSE_DEC mdec;
   struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
   struct SHTCTL *shtctl;
-  struct SHEET *sht_back, *sht_mouse;
-  unsigned char *buf_back, buf_mouse[256];
+  struct SHEET *sht_back, *sht_mouse, *sht_win;
+  unsigned char *buf_back, buf_mouse[256], *buf_win;
 
   init_gdtidt();
   init_pic();
   io_sti(); // IDT/PICの初期化が終わったのでCPUの割り込み禁止を解除
-
   fifo8_init(&keyfifo, 32, keybuf);
   fifo8_init(&mousefifo, 128, mousebuf);
   io_out8(PIC0_IMR, 0xf9); // PIC1とキーボードを許可(11111001)
   io_out8(PIC1_IMR, 0xef); // マウスを許可(11101111)
-
   init_keyboard();
   enable_mouse(&mdec);
 
@@ -33,28 +33,33 @@ void HariMain(void)
   memman_free(memman, 0x00400000, memtotal - 0x00400000);
 
   init_palette(); /* パレットを設定 */
-
   shtctl = shtctl_init(memman, binfo->vram, binfo->scrnx, binfo->scrny);
   sht_back = sheet_alloc(shtctl);
   sht_mouse = sheet_alloc(shtctl);
+  sht_win = sheet_alloc(shtctl);
   buf_back = (unsigned char *) memman_alloc_4k(memman, binfo->scrnx * binfo->scrny);
+  buf_win = (unsigned char *) memman_alloc_4k(memman, 160 * 68);
   sheet_setbuf(sht_back, buf_back, binfo->scrnx, binfo->scrny, -1); // 透明色なし
   sheet_setbuf(sht_mouse, buf_mouse, 16, 16, 99);
-
+  sheet_setbuf(sht_win, buf_win, 160, 68, -1); // 透明色なし
   init_screen8(buf_back, binfo->scrnx, binfo->scrny);
   init_mouse_cursor8(buf_mouse, 99);
-  sheet_slide(sht_back, 0, 0);
 
+  make_window8(buf_win, 160, 68, "window");
+  putfonts8_asc(buf_win, 160, 24, 28, COL8_000000, "Welcome to");
+  putfonts8_asc(buf_win, 160, 24, 44, COL8_000000, "  Haribote-OS!");
+
+  sheet_slide(sht_back, 0, 0);
   mx = (binfo->scrnx - 16) / 2; /* 画面中央になるように座標計算 */
   my = (binfo->scrny - 28 - 16) / 2;
-
   sheet_slide(sht_mouse, mx, my);
-  sheet_updown(sht_back, 0);
-  sheet_updown(sht_mouse, 1);
+  sheet_slide(sht_win, 80, 72);
+  sheet_updown(sht_back,  0);
+  sheet_updown(sht_win,   1);
+  sheet_updown(sht_mouse, 2);
 
   sprintf(s, "(%3d, %3d)", mx, my);
   putfonts8_asc(buf_back, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
-
   sprintf(s, "memory %dMB   free : %dKB",
       memtotal / (1024 * 1024), memman_total(memman) / 1024);
   putfonts8_asc(buf_back, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
@@ -115,5 +120,54 @@ void HariMain(void)
       }
     }
   }
+}
+
+void make_window8(unsigned char *buf, int xsize, int ysize, char *title)
+{
+  static char closebtn[14][16] = {
+    "OOOOOOOOOOOOOOO@",
+		"OQQQQQQQQQQQQQ$@",
+		"OQQQQQQQQQQQQQ$@",
+		"OQQQ@@QQQQ@@QQ$@",
+		"OQQQQ@@QQ@@QQQ$@",
+		"OQQQQQ@@@@QQQQ$@",
+		"OQQQQQQ@@QQQQQ$@",
+		"OQQQQQ@@@@QQQQ$@",
+		"OQQQQ@@QQ@@QQQ$@",
+		"OQQQ@@QQQQ@@QQ$@",
+		"OQQQQQQQQQQQQQ$@",
+		"OQQQQQQQQQQQQQ$@",
+		"O$$$$$$$$$$$$$$@",
+		"@@@@@@@@@@@@@@@@"
+  };
+  int x, y;
+  char c;
+  boxfill8(buf, xsize, COL8_C6C6C6, 0,         0,         xsize - 1, 0        );
+  boxfill8(buf, xsize, COL8_FFFFFF, 1,         1,         xsize - 2, 1        );
+  boxfill8(buf, xsize, COL8_C6C6C6, 0,         0,         0,         ysize - 1);
+  boxfill8(buf, xsize, COL8_FFFFFF, 1,         1,         1,         ysize - 1);
+  boxfill8(buf, xsize, COL8_848484, xsize - 2, 1,         xsize - 2, ysize - 2);
+  boxfill8(buf, xsize, COL8_000000, xsize - 1, 0,         xsize - 1, ysize - 1);
+  boxfill8(buf, xsize, COL8_C6C6C6, 2,         2,         xsize - 3, ysize - 3);
+  boxfill8(buf, xsize, COL8_000084, 3,         3,         xsize - 4, 20);
+  boxfill8(buf, xsize, COL8_848484, 1,         ysize - 2, xsize - 2, ysize - 2);
+  boxfill8(buf, xsize, COL8_000000, 0,         ysize - 1, xsize - 1, ysize - 1);
+  putfonts8_asc(buf, xsize, 24, 4, COL8_FFFFFF, title);
+  for (y = 0; y < 14; y++) {
+    for (x = 0; x < 16; x++) {
+      c = closebtn[y][x];
+      if (c == '@') {
+        c = COL8_000000;
+      } else if (c == '$') {
+        c = COL8_848484;
+      } else if (c == 'Q') {
+        c = COL8_C6C6C6;
+      } else {
+        c = COL8_FFFFFF;
+      }
+      buf[(5 + y) * xsize + (xsize - 21 + x)] = c;
+    }
+  }
+  return;
 }
 
