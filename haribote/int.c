46b80916a3218a3b9b874b6117be56f221a4a629
Cashew <42924021+cashev@users.noreply.github.com>
2020-08-25 09:27:13 -0700

    p593 harib24g make環境の整理

--- haribote/int.c
+++ haribote/int.c
@@ -11,7 +11,7 @@ void init_pic(void)
 
   io_out8(PIC0_ICW1, 0x11  ); /* エッジトリガモード */
   io_out8(PIC0_ICW2, 0x20  ); /* IRQ0-7は、INT20-27で受ける */
-  io_out8(PIC0_ICW1, 1 << 2); /* PIC1はIRQ2に接続 */
+  io_out8(PIC0_ICW3, 1 << 2); /* PIC1はIRQ2に接続 */
   io_out8(PIC0_ICW4, 0x01  ); /* ノンバッファモード */
 
   io_out8(PIC1_ICW1, 0x11  ); /* エッジトリガモード */
